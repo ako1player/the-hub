e8c60a7fd3ae83a0694f09053e4cf5f9795f2ca2
Adrian Garcia Rios <adrigar10@gmail.com>
2023-10-09 09:20:02 -0400

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,12 +4,12 @@ import 'firebase/compat/auth'
 import 'firebase/compat/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCxn7izDShN8Do5nrYTQSiu54sNN2G7Pvk",
-    authDomain: "thehubsite-c06f0.firebaseapp.com",
-    projectId: "thehubsite-c06f0",
-    storageBucket: "thehubsite-c06f0.appspot.com",
-    messagingSenderId: "7676647490",
-    appId: "1:7676647490:web:b26339988f711700a7b5db"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   //init firebase
